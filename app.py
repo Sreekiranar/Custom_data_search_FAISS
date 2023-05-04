ebb8e15077c8e789d6a3c9683800340f70d88189
Sreekiranar <sreekiranar@gmail.com>
2023-05-04 19:09:25 +0100

    PDF parsing activated using PyMuPDF

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 import faiss
 import openai
-import pdfplumber
 import numpy as np
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from sentence_transformers import SentenceTransformer
-
+import fitz
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 app = Flask(__name__, static_folder=".", static_url_path="")
@@ -30,10 +29,8 @@ def home():
     """Serve the home page."""
     return send_from_directory(".", "index.html")
 
-
 @app.route("/upload", methods=["POST"])
 def upload():
-    """Upload a file containing rules and index them using SentenceTransformer embeddings."""
     global rules, rules_embeddings, index
 
     if "file" not in request.files:
@@ -43,10 +40,17 @@ def upload():
     filename = secure_filename(file.filename)
     file.save(filename)
 
-    rules = []
-
-    with open(filename, "r") as f:
-        rules = [line.strip() for line in f.readlines()]
+    if filename.endswith(".txt"):
+        with open(filename, "r") as f:
+            rules = [line.strip() for line in f.readlines()]
+    elif filename.endswith(".pdf"):
+        rules = []
+        with fitz.open(filename) as pdf_file:
+            for page in pdf_file:
+                text = page.get_text()
+                print(f"{text=}")
+                rules.extend([line.strip() for line in text.split('\n') if line.strip()])
+                print([line.strip() for line in text.split('\n') if line.strip()])
 
     print(f"Rules uploaded: {len(rules)}")
 
@@ -92,32 +96,35 @@ def query():
     })
 
 
-def answer_query_openai_api(query, relevant_rules):
+def answer_query_openai_api(query, relevant_rules, mock_response = True):
     """
     Answer a query using the OpenAI API with the given relevant rules.
 
     Args:
         query (str): The query to be answered.
         relevant_rules (list): A list of relevant rules (strings).
-
+        mock_response (boolean): Flag to activate mock reponse for testing
     Returns:
         str: The answer provided by the OpenAI API.
     """
     context = " ".join(relevant_rules)
+    if not mock_response:
 
-    prompt = f"Question: {query}\nI need an accurate and compact answer from the following information, if not inform:\n{context}\n\nAnswer:"
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=100,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=["\n"],
-    )
+        prompt = f"Question: {query}\nI need an accurate and compact answer from the following information, if not inform:\n{context}\n\nAnswer:"
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=100,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\n"],
+        )
 
-    answer = response.choices[0].text.strip()
+        answer = response.choices[0].text.strip()
+    else:
+        answer = 'mock answer'
     print(f"answer={answer}")
     return answer
 
